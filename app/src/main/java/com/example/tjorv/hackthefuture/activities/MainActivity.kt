22286a082cd16e5696a8af909ed9204a42829359
Unknown <vincent_coutuer@yahoo.com>
2018-11-29 11:29:44 +0100

    Login scherm

--- app/src/main/java/com/example/tjorv/hackthefuture/activities/MainActivity.kt
+++ app/src/main/java/com/example/tjorv/hackthefuture/activities/MainActivity.kt
@@ -3,11 +3,28 @@ package com.example.tjorv.hackthefuture.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tjorv.hackthefuture.R
+import com.example.tjorv.hackthefuture.fragments.LoginFragment
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Start de fragmentmanager die ervoor gaat zorgen dat de juiste fragment getoont wordt
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        //eerste fragment is momenteel homepage (zal login worden)
+        val fragment = LoginFragment()
+
+        fragmentTransaction.add(R.id.fragment_holder, fragment)
+        fragmentTransaction.commit()
+
+
     }
 }
+
+
+
+
